b9ce7a85a2aad050bd0c6ad1bd00e224dd4b475b
Nisha Sinha <sinhagautam488@gmail.com>
2025-12-03 14:32:00 +0530

    config folder added

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    console.log("Connecting to MongoDB...");
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
+    console.log("MongoDB Connection Established!");
+  } catch (error) {
+    console.log("Full Error Details:", error);
+  }
+};
+
+export default connectDb;
